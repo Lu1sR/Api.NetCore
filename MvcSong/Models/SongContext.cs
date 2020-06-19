2ccf0465f0e1cdd44fd8cb884a846a37355d841d
Luis <luishrt1997@gmail.com>
2020-06-19 12:25:59 -0500

    First Commit

---
+++ MvcSong/Models/SongContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcSong.Models
+{
+    public class SongContext : DbContext
+    {
+        public SongContext(DbContextOptions<SongContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Song> TodoItems { get; set; }
+    }
+}
